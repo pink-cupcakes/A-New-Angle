e1d0ff5a0767b2648a8df8a967074415bb004b66
andy <chaochaoqi@gmail.com>
2018-05-19 08:38:18 -0700

    Toggle text expander on and off

--- src/components/questionEntry.js
+++ src/components/questionEntry.js
@@ -2,7 +2,8 @@ angular.module('forum')
 
 .controller('ProcessAnswer', function($scope) {
     this.expandQuestion = () => {
-        document.getElementById(`questionID${this.questionId}`).classList.add('active');
+        // document.getElementById(`questionID${this.questionId}`).classList.add('active');
+        $(`#questionID${this.questionId}`).toggleClass("active");
     };
 
     this.submitComment = (id, text) => {
